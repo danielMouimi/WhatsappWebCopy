f632fc73a371965d0c33e49df33a4733892af6de
Daniel Mouimi <mouimidaniel@gmail.com>
2025-02-27 02:40:15 +0100

    last updates

--- back/servidor/index.js
+++ back/servidor/index.js
@@ -10,7 +10,8 @@ const server = http.createServer(app);
 const io = new Server(server, {
     cors: {
         origin: "https://whatsappwebcopy-1.onrender.com",  //  
-        methods: ["GET", "POST"]
+        methods: ["GET", "POST"],
+        credentials: true
     }
 });
 
