4484b70705d123431a85c3a9af3ec39f443d837a
Daniel Mouimi <mouimidaniel@gmail.com>
2025-02-27 01:56:16 +0100

    last updates

--- back/servidor/index.js
+++ back/servidor/index.js
@@ -9,7 +9,7 @@ const port = 3000;
 const server = http.createServer(app);
 const io = new Server(server, {
     cors: {
-        origin: "http://localhost:5173", 
+        origin: process.env.CLIENT_URL || "http://localhost:3000",  //  
         methods: ["GET", "POST"]
     }
 });
