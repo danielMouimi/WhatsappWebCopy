f80689ffd298fbdf694d70636621a437b68a5d8b
Daniel Mouimi <mouimidaniel@gmail.com>
2025-02-27 01:14:26 +0100

    last updates

--- front/src/Chat.jsx
+++ front/src/Chat.jsx
@@ -3,7 +3,9 @@ import { io } from "socket.io-client";
 import { useLocation, useNavigate } from "react-router-dom";
 import "./Chat.css"; // Importa la hoja de estilos
 
-const socket = io("http://localhost:3000");
+const socket = io(
+  process.env.NODE_ENV === "production" ? "https://nodeprojectqueapp.onrender.com" : "http://localhost:3000"
+);
 
 export default function Chat() {
   const [messages, setMessages] = useState([]);
